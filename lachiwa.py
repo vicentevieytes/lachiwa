4aed3bc084867fe57dfa13a03546eca14f2b8417
vicente <vicentevieytes@hotmail.c>
2024-05-26 12:09:20 -0300

    refactor

--- lachiwa.py
+++ lachiwa.py
@@ -1,67 +1,57 @@
-import click
 import uuid
 from datetime import datetime
 from qrcode.main import QRCode
-import qrcode
+from qrcode.constants import ERROR_CORRECT_L
 
-def create_qr_code(url, file_name):
-    # Create a QR code object
-    qr = QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    # Add data to the QR code
-    qr.add_data(url)
-    qr.make(fit=True)
-    # Create an image from the QR code object
-    img = qr.make_image(fill_color="black", back_color="white")
-    # Save the image to a file
-    img.save(file_name)
-    print(f"QR code saved as {file_name}")
+def url_from_host_and_tokenid(host, id, protocol="http"): 
+    return f"{protocol}://{host}/id?={id}"
 
 class Token:
-    def __init__(self, host, description, email, token_type):
+    def __init__(self, host: str, description: str, email: str, token_type: str):
         self.host = host
         self.description = description
         self.email = email
         self.token_type = token_type
         self.created_at = datetime.now()
         self.id = uuid.uuid4()
-
+    
     def __str__(self):
         return (f"Token(host={self.host}, description={self.description}, email={self.email}, "
                 f"token_type={self.token_type}, created_at={self.created_at}, id={self.id})")
 
+    
+
 class URLToken(Token):
     def __init__(self, host, description, email):
         super().__init__(host, description, email,"URLToken")
-        self.url = f"http://{self.host}/id?={self.id}"
+        self.url = url_from_host_and_tokenid(self.host, self.id, "http")
 
     def __str__(self):
         return (f"URLToken(host={self.host}, description={self.description}, email={self.email}, "
                 f"token_type={self.token_type}, created_at={self.created_at}, id={self.id}, url={self.url})")
 
+
 class QRToken(Token):
-    def __init__(self, host, description, email):
+    def __init__(self, host: str, description :str, email: str):
         super().__init__(host, description, email, "QRToken")
-        self.qr = create_qr_code(f"http://{self.host}/id?={self.id}", f"honeytokens/QR{datetime.today()}.jpg")
-        
-@click.command()
-@click.option('--host', prompt='Host', help='The host for the honeytoken.')
-@click.option('--description', prompt='Description', help='Description of the honeytoken.')
-@click.option('--email', prompt='Email', help='Email associated with the honeytoken.')
-@click.option('--token_type', prompt='Token Type', help='Type of the honeytoken.')
-def create_honeytoken(host, description, email, token_type):
-    if token_type == "url": 
-        token = URLToken(host, description, email)
-        click.echo(f"Generated URL: {token.url}")
-        click.echo(f"Token Details: {token}")
-    elif token_type == "qr":
-        token = QRToken(host, description, email)
-    elif token_type == "exe":
-        token = EXEToken(host, description, email)
-        
-if __name__ == '__main__':
-    create_honeytoken()
+        self.filename = f"QR_{description}_{datetime.today()}.jpg"
+        self.create_qr_code(url_from_host_and_tokenid(self.host, self.id), f"honeytokens/{self.filename}")
+
+    def create_qr_code(self, url: str, file_name :str):
+        # Create a QR code object
+        qr = QRCode(
+            version=1,
+            error_correction=ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        # Add data to the QR code
+        qr.add_data(url)
+        qr.make(fit=True)
+        # Create an image from the QR code object
+        img = qr.make_image(fill_color="black", back_color="white")
+        # Save the image to a file
+        img.save(file_name)
+
+
+
---
+++ lachiwa_cli.py
@@ -0,0 +1,28 @@
+import click
+from lachiwa import Token, URLToken, QRToken
+@click.command()
+@click.option('--host', prompt='Host', help='The host for the honeytoken.')
+@click.option('--description', prompt='Description', help='Description of the honeytoken.')
+@click.option('--email', prompt='Email', help='Email associated with the honeytoken.')
+@click.option('--token_type', prompt='Token Type', help='Type of the honeytoken.')
+def create_honeytoken(host: str, description: str, email: str, token_type: str):
+    return from_token_type_str(host, description, email, token_type)
+
+def from_token_type_str(host: str, description: str, email: str, token_type: str) -> "Token":
+    match token_type:
+        case "url":
+            token = URLToken(host, description, email)
+            click.echo(f"Generated URL: {token.url}")
+            click.echo(f"Token Details: {token}")
+            return token
+        case "qr":
+            token = QRToken(host, description, email)
+            click.echo(f"Generated QR: {token.filename}")
+            click.echo(f"Token Details: {token}")
+            return token
+        case _:
+            print("Wrong token type indicated")
+            exit(1)
+
+if __name__ == '__main__':
+    create_honeytoken()
