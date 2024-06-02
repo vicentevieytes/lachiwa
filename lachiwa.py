4c3b6864dcb8fa5391f2d9a7a6ebd126b4497544
tomas <temanuel@fi.uba.ar>
2024-06-02 15:43:34 -0300

    Base commit for excel token

--- lachiwa.py
+++ lachiwa.py
@@ -2,9 +2,12 @@ from datetime import datetime
 from qrcode.main import QRCode
 from qrcode.constants import ERROR_CORRECT_L
 import nanoid
+from openpyxl import Workbook
 
-def url_from_host_and_tokenid(host, id, protocol="http"): 
+
+def url_from_host_and_tokenid(host, id, protocol="http"):
     return f"{protocol}://{host}/?id={id}"
+
 
 class Token:
     def __init__(self, host: str, description: str, email: str, token_type: str):
@@ -14,16 +17,15 @@ class Token:
         self.token_type = token_type
         self.timestamp = datetime.now()
         self.id = nanoid.generate(size=10)
-    
+
     def __str__(self):
         return (f"Token(host={self.host}, description={self.description}, email={self.email}, "
                 f"token_type={self.token_type}, timestamp={self.timestamp}, id={self.id})")
 
-    
 
 class URLToken(Token):
     def __init__(self, host, description, email):
-        super().__init__(host, description, email,"URLToken")
+        super().__init__(host, description, email, "URLToken")
         self.url = url_from_host_and_tokenid(self.host, self.id, "http")
         with open(f"honeytokens/URL_{description}_{datetime.today()}", "w") as output_file:
             output_file.write(self.url)
@@ -34,12 +36,13 @@ class URLToken(Token):
 
 
 class QRToken(Token):
-    def __init__(self, host: str, description :str, email: str):
+    def __init__(self, host: str, description: str, email: str):
         super().__init__(host, description, email, "QRToken")
         self.filename = f"QR_{description}_{datetime.today()}.jpg"
-        self.create_qr_code(url_from_host_and_tokenid(self.host, self.id), f"honeytokens/{self.filename}")
+        self.create_qr_code(url_from_host_and_tokenid(
+            self.host, self.id), f"honeytokens/{self.filename}")
 
-    def create_qr_code(self, url: str, file_name :str):
+    def create_qr_code(self, url: str, file_name: str):
         # Create a QR code object
         qr = QRCode(
             version=1,
@@ -56,4 +59,14 @@ class QRToken(Token):
         img.save(file_name)
 
 
+class ExcelToken(Token):
 
+    def __init__(self, host: str, description: str, email: str):
+        super().__init__(host, description, email, "ExcelToken")
+        self.filename = f"Excel_{description}_{datetime.today()}.xlsx"
+        self.create_excel_file(f"honeytokens/{self.filename}")
+
+    def create_excel_file(self, file_name: str):
+        workbook = Workbook()
+        workbook.active
+        workbook.save(file_name)
--- lachiwa_cli.py
+++ lachiwa_cli.py
@@ -1,14 +1,15 @@
 import click
 from trogon import tui
-from lachiwa import Token, URLToken, QRToken
+from lachiwa import ExcelToken, Token, URLToken, QRToken
 import redis
 import os
+
 
 def save_to_redis(token: Token):
     redis_host = os.getenv('REDIS_HOST', 'localhost')
     redis_port = int(os.getenv('REDIS_PORT', 6379))
-    #Redis connection
-    redis_client = redis.StrictRedis(host=redis_host,port=redis_port, db=0)
+    # Redis connection
+    redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
     try:
         token_key = f"Token:{token.id}"
         token_data = {
@@ -27,14 +28,16 @@ def save_to_redis(token: Token):
         # Ensure the Redis connection is closed properly
         redis_client.close()
 
+
 @tui()
 @click.command()
-@click.option('--host', prompt='Host', help='The host for the honeytoken.', type= str)
-@click.option('--description', prompt='Description', help='Description of the honeytoken.', type= str)
-@click.option('--email', prompt='Email', help='Email associated with the honeytoken.',type= str)
-@click.option('--token_type', prompt='Token Type', help='Type of the honeytoken.',type= str)
+@click.option('--host', prompt='Host', help='The host for the honeytoken.', type=str)
+@click.option('--description', prompt='Description', help='Description of the honeytoken.', type=str)
+@click.option('--email', prompt='Email', help='Email associated with the honeytoken.', type=str)
+@click.option('--token_type', prompt='Token Type', help='Type of the honeytoken.', type=str)
 def create_honeytoken(host: str, description: str, email: str, token_type: str):
     return from_token_type_str(host, description, email, token_type)
+
 
 def from_token_type_str(host: str, description: str, email: str, token_type: str) -> "Token":
     match token_type:
@@ -50,9 +53,16 @@ def from_token_type_str(host: str, description: str, email: str, token_type: str
             click.echo(f"Generated QR: {token.filename}")
             click.echo(f"Token Details: {token}")
             return token
+        case "excel":
+            token = ExcelToken(host, description, email)
+            save_to_redis(token)
+            click.echo(f"Generated Excel: {token.filename}")
+            click.echo(f"Token Details: {token}")
+            return token
         case _:
             print("Wrong token type indicated")
             exit(1)
+
 
 if __name__ == '__main__':
     create_honeytoken()
