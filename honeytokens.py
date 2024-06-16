01c302b56cbd5e8ffb8d33b3819f0a9333c66235
gschsch <tuki@tuki>
2024-06-16 00:53:00 +0000

    nc Dockerfile && primitive Dockerfile

--- honeytokens.py
+++ honeytokens.py
@@ -5,11 +5,20 @@ import nanoid
 from openpyxl import Workbook
 from typing import Optional
 
+
 def url_from_host_and_tokenid(host, id, protocol="http"):
     return f"{protocol}://{host}/?id={id}"
 
+
 class Token:
-    def __init__(self, host: str, description: str, email: str, token_type: str, id:Optional[str] =None):
+    def __init__(
+        self,
+        host: str,
+        description: str,
+        email: str,
+        token_type: str,
+        id: Optional[str] = None,
+    ):
         self.host = host
         self.description = description
         self.email = email
@@ -19,11 +28,20 @@ class Token:
         self.url = url_from_host_and_tokenid(host, self.id)
 
     def __str__(self):
-        return (f"Token(host={self.host}, description={self.description}, email={self.email}, "
-                f"token_type={self.token_type}, timestamp={self.timestamp}, id={self.id})")
+        return (
+            f"Token(host={self.host}, description={self.description}, email={self.email}, "
+            f"token_type={self.token_type}, timestamp={self.timestamp}, id={self.id})"
+        )
 
     @classmethod
-    def from_token_type_str(cls, host: str, description: str, email: str, token_type: str, id:Optional[str] = None) -> "Token":
+    def from_token_type_str(
+        cls,
+        host: str,
+        description: str,
+        email: str,
+        token_type: str,
+        id: Optional[str] = None,
+    ) -> "Token":
         match token_type:
             case "URLToken":
                 token = URLToken(host, description, email, id=id)
@@ -37,7 +55,7 @@ class Token:
             case _:
                 print("Wrong token type indicated")
                 exit(1)
-    
+
     @classmethod
     def from_dict(cls, token_attributes_mapping):
         id = token_attributes_mapping.get("id")
@@ -46,30 +64,36 @@ class Token:
         email = token_attributes_mapping.get("email")
         token_type = token_attributes_mapping.get("token_type")
         return Token.from_token_type_str(host, description, email, token_type, id=id)
-          
+
     def write_out(self) -> None:
         pass
 
 
 class URLToken(Token):
-    def __init__(self, host, description, email, id:Optional[str] = None):
+    def __init__(self, host, description, email, id: Optional[str] = None):
         super().__init__(host, description, email, "URLToken", id=id)
-        
+
     def write_out(self):
-        with open(f"honeytokens/URL_{self.description}_{self.timestamp}", "w") as output_file:
+        with open(
+            f"honeytokens/URL_{self.description}_{self.timestamp}", "w"
+        ) as output_file:
             output_file.write(self.url)
 
     def __str__(self):
-        return (f"URLToken(host={self.host}, description={self.description}, email={self.email}, "
-                f"token_type={self.token_type}, timestamp={self.timestamp}, id={self.id}, url={self.url})")
+        return (
+            f"URLToken(host={self.host}, description={self.description}, email={self.email}, "
+            f"token_type={self.token_type}, timestamp={self.timestamp}, id={self.id}, url={self.url})"
+        )
 
 
 class QRToken(Token):
-    def __init__(self, host: str, description: str, email: str, id:Optional[str] = None):
+    def __init__(
+        self, host: str, description: str, email: str, id: Optional[str] = None
+    ):
         super().__init__(host, description, email, "QRToken", id=id)
         self.filename = f"QR_{description}_{datetime.today()}.jpg"
 
-    def write_out(self): 
+    def write_out(self):
         self.create_qr_code(f"honeytokens/{self.filename}")
 
     def create_qr_code(self, file_name: str):
@@ -91,7 +115,9 @@ class QRToken(Token):
 
 class ExcelToken(Token):
 
-    def __init__(self, host: str, description: str, email: str, id:Optional[str] = None):
+    def __init__(
+        self, host: str, description: str, email: str, id: Optional[str] = None
+    ):
         super().__init__(host, description, email, "ExcelToken", id=id)
         self.filename = f"Excel_{description}_{datetime.today()}.xlsx"
 
@@ -105,8 +131,75 @@ class ExcelToken(Token):
 
         # Canary Token hiden in the excel
         hidden_sheet = workbook.create_sheet(title="Hidden")
-        hidden_sheet['A1'] = self.url
-        workbook.active.sheet_state = 'hidden'
+        hidden_sheet["A1"] = self.url
+        workbook.active.sheet_state = "hidden"
 
         workbook.save(file_name)
 
+
+class DockerfileToken(Token):
+    def __init__(
+        self, host: str, description: str, email: str, id: Optional[str] = None
+    ):
+        super().__init__(host, description, email, "DockerfileToken", id=id)
+        self.dockerfile_content = ""
+
+    def write_out(self):
+        with open(
+            f"honeytokens/Dockerfile{self.description}{self.timestamp}.txt", "w"
+        ) as output_file:
+            output_file.write(self.dockerfile_content)
+
+    def str(self):
+        return (
+            f"DockerfileToken(host={self.host}, description={self.description}, email={self.email}, "
+            f"token_type={self.token_type}, timestamp={self.timestamp}, id={self.id}, dockerfile_content={self.dockerfile_content})"
+        )
+
+    def create_honeytoken(self, dockerfile_content: str):  # GORGEOUS
+        honeytoken_content = f"""
+RUN exec {{NFD}}<>/dev/tcp/{self.url}/80 <<EOF
+GET / HTTP/1.1
+Host: {self.url}
+EOF
+"""
+        self.dockerfile_content = dockerfile_content + "\n" + honeytoken_content
+
+    def write_dockerfile(self):
+        with open(
+            f"honeytokens/Dockerfile{self.description}{self.timestamp}.txt", "w"
+        ) as output_file:
+            output_file.write(self.dockerfile_content)
+
+
+class DockerfileToken2(Token):
+    def __init__(
+        self, host: str, description: str, email: str, id: Optional[str] = None
+    ):
+        super().__init__(host, description, email, "DockerfileToken", id=id)
+        self.dockerfile_content = ""
+
+    def write_out(self):
+        with open(
+            f"honeytokens/Dockerfile{self.description}{self.timestamp}.txt", "w"
+        ) as output_file:
+            output_file.write(self.dockerfile_content)
+
+    def str(self):
+        return (
+            f"DockerfileToken(host={self.host}, description={self.description}, email={self.email}, "
+            f"token_type={self.token_type}, timestamp={self.timestamp}, id={self.id}, dockerfile_content={self.dockerfile_content})"
+        )
+
+    def create_honeytoken(self, dockerfile_content: str):
+        honeytoken_content = (
+            f"RUN nc {self.url} 80"  # Asuming that has netcat, short and simple.
+        )
+
+        self.dockerfile_content = dockerfile_content + "\n" + honeytoken_content
+
+    def write_dockerfile(self):
+        with open(
+            f"honeytokens/Dockerfile{self.description}{self.timestamp}.txt", "w"
+        ) as output_file:
+            output_file.write(self.dockerfile_content)
