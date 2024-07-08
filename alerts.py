856108589f9e38e6ad37253d322d87e9bb0281c6
vicente <vicentevieytes@hotmail.c>
2024-07-08 19:58:34 -0300

    make storage and retrieval of tokens work, now having problems with storing alerts

--- alerts.py
+++ alerts.py
@@ -4,19 +4,18 @@ from redis_om import HashModel, Field
 
 class Alert(HashModel):
     timestamp: datetime = Field(index = True)
-    token_id: str = Field(index = True)
+    token_id: str | None = Field(index = True)
     token_type: str = Field(index = True)
     token_description: str
     remote_ip: str | None = Field(index= True)
     
-    def __init__(self, token: Token, remote_ip: str | None):
-        print(token)
+    def __init__(self, token: Token, remote_ip: str | None, **kwargs):
+        super().__init__(**kwargs)
         self.token_id = token.pk
         self.token_description = token.description
-        self.token_type = token.token_type
+        self.token_type = token.__class__.__name__
         self.timestamp = datetime.now()
-        self.remote_ip = remote_ip
-        
+        self.remote_ip = remote_ip       
 
     def log_alert(self):
         with open(f"alerts/alert_log", "a") as alert_log:
--- redismanager.py
+++ redismanager.py
@@ -3,20 +3,27 @@ from datetime import datetime
 from honeytokens import Token, URLToken
 from alerts import Alert
 from typing import Awaitable, Union, Optional
-from redis_om import NotFoundError
+from redis_om import get_redis_connection, NotFoundError
 
 def store_token(token: Token):
-    print(token)
+    redis_conn = get_redis_connection(port = 6379)
     token.save()
+        # Store the type information
+    redis_conn.set(f"token_type:{token.pk}", token.__class__.__name__)
 
 def fetch_token(token_id: str) -> Token|None:
-    try:
-        token= Token.get(token_id)
-        print(token)
-        return(token)
-    except NotFoundError:
-        print("not found token")
-        return None
+    redis_conn = get_redis_connection(port = 6379)
+    token_type = redis_conn.get(f"token_type:{token_id}")
+    if not token_type:
+        raise ValueError("Token not found")
+
+    # Get the corresponding class
+    token_class = globals().get(token_type)
+    if not token_class:
+        raise ValueError("Invalid token type")
+
+    # Query the token
+    return token_class.get(token_id)
 
 def store_alert(alert: Alert):
     alert.save()
