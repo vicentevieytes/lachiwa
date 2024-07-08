b391cb24814f44867f91f6dbbb6de62504381e35
vicente <vicentevieytes@hotmail.c>
2024-07-07 23:03:58 -0300

    refactor to use redis-om, apply on Token and URLToken for saving. not working for fetching

--- alerts.py
+++ alerts.py
@@ -1,20 +1,26 @@
 from honeytokens import Token
 from datetime import datetime
+from redis_om import HashModel, Field
 
-class Alert:
-    def __init__(self, token: Token):
-        self.token = token
-        self.id = token.id
+class Alert(HashModel):
+    timestamp: datetime = Field(index = True)
+    token_id: str = Field(index = True)
+    token_type: str = Field(index = True)
+    token_description: str
+    remote_ip: str | None = Field(index= True)
+    
+    def __init__(self,token: Token, remote_ip: str | None):
+        
+        self.token_id = token.pk
+        self.token_description = token.description
         self.token_type = token.token_type
-        self.host = token.host
-        self.token_timestamp = token.timestamp
-        self.email = token.email
-        self.description = token.description
         self.timestamp = datetime.now()
+        self.remote_ip = remote_ip
+        
 
     def log_alert(self):
         with open(f"alerts/alert_log", "a") as alert_log:
-            alert_log.write(f"{self.timestamp} {self.token_type} Token:{self.id} Creation Timestamp: {self.token_timestamp} Description: {self.description}\n")
+            alert_log.write(f"{self.timestamp} {self.token_type} Token:{self.token_id} Remote_ip:{self.remote_ip} Description: {self.description}\n")
 
 #TODO: def send_alert_email(self):
 
