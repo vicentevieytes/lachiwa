f60d9f9bba46933d1f2b2b59e5e76191df2b7eef
tomas <temanuel@fi.uba.ar>
2024-07-11 21:07:41 -0300

    add honeytokens.py for testing

--- honeytokens.py
+++ honeytokens.py
@@ -12,8 +12,10 @@ import os
 from redis_om import HashModel, Field
 from abc import ABC
 
+
 def url_from_host_and_tokenid(host, id, protocol="http"):
     return f"{protocol}://{host}/?id={id}"
+
 
 class Token(HashModel):
     host: str
@@ -28,19 +30,21 @@ class Token(HashModel):
 
     def __str__(self):
         return (
-            f"Token(host={self.host}, description={self.description}, email={self.email}, "
-            f"token_type={self.token_type}, timestamp={self.timestamp}, id={self.pk})"
+            f"Token(host={self.host}, description={
+                self.description}, email={self.email}, "
+            f"token_type={self.token_type}, timestamp={
+                self.timestamp}, id={self.pk})"
         )
 
     def url(self):
         return url_from_host_and_tokenid(self.host, self.pk)
-    
+
     def write_out(self) -> None:
         pass
 
 
 class URLToken(Token):
-    token_type: str = Field(default = "URLToken", index = True)
+    token_type: str = Field(default="URLToken", index=True)
 
     def write_out(self):
         with open(
@@ -52,14 +56,16 @@ class URLToken(Token):
         return f"URL_{self.description}.txt"
 
     def __str__(self):
-        return ( 
-            f"URLToken(host={self.host}, description={self.description}, email={self.email}, "
-            f"token_type={self.token_type}, timestamp={self.timestamp}, id={self.pk}, url={url_from_host_and_tokenid(self.host, self.pk)})"
+        return (
+            f"URLToken(host={self.host}, description={
+                self.description}, email={self.email}, "
+            f"token_type={self.token_type}, timestamp={self.timestamp}, id={
+                self.pk}, url={url_from_host_and_tokenid(self.host, self.pk)}"
         )
 
 
 class QRToken(Token):
-    token_type:str = Field(default = "QRToken", index = True)
+    token_type: str = Field(default="QRToken", index=True)
 
     def write_out(self):
         self.create_qr_code(f"honeytokens/{self.filename()}")
@@ -83,49 +89,49 @@ class QRToken(Token):
     def filename(self):
         return f"QR_{self.description}.jpg"
 
+
 class ExcelToken(Token):
-    token_type:str = Field(default = "ExcelToken", index = True)
+    token_type: str = Field(default="ExcelToken", index=True)
 
     def write_out(self):
         # Create an initial Excel template using openpyxl
         filepath = self.filename()
         wb = Workbook()
         ws = wb.active
-        ws['A1'] = "This is a test"
+        ws['A1'] = "This is a test for Excel Token."
         ws['A1'].style = 'Title'
         wb.save(filepath)
-        old_string = 'xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"'
-        url = f'''xmlns="{url_from_host_and_tokenid(self.host, self.pk)}"'''
-        modified_file = f"honeytokens/modified_{self.filename()}"
-        try:
-            shutil.copy(filepath, modified_file)
-            extracted_dir = f"honeytokens/extracted_{self.filename}"
-            with zipfile.ZipFile(modified_file, 'r') as zip_ref:
-                zip_ref.extractall(extracted_dir)
+        # old_string = 'xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"'
+        # url = f'''xmlns="{url_from_host_and_tokenid(self.host, self.pk)}"'''
+        # modified_file = f"honeytokens/modified_{self.filename()}"
+        # try:
+        #     shutil.copy(filepath, modified_file)
+        # extracted_dir = f"honeytokens/extracted_{self.filename()}"
+        #     with zipfile.ZipFile(modified_file, 'r') as zip_ref:
+        #         zip_ref.extractall(extracted_dir)
 
-            styles_path = os.path.join(extracted_dir, 'xl', 'styles.xml')
-            with open(styles_path, 'r', encoding='utf-8') as file:
-                styles_content = file.read()
+        # styles_path = os.path.join(extracted_dir, 'xl', 'styles.xml')
+        #     with open(styles_path, 'r', encoding='utf-8') as file:
+        #         styles_content = file.read()
 
-            new_styles_content = styles_content.replace(old_string, url)
-            with open(styles_path, 'w', encoding='utf-8') as file:
-                file.write(new_styles_content)
+        #     new_styles_content = styles_content.replace(old_string, url)
+        #     with open(styles_path, 'w', encoding='utf-8') as file:
+        #         file.write(new_styles_content)
 
-            new_zip_file = f"honeytokens/modified_{self.filename}.xlsx"
-            with zipfile.ZipFile(new_zip_file, 'w') as zipf:
-                for root, _, files in os.walk(extracted_dir):
-                    for file in files:
-                        file_path = os.path.join(root, file)
-                        zipf.write(file_path, os.path.relpath(
-                            file_path, extracted_dir))
+        new_zip_file = f"honeytokens/modified_{self.filename}.xlsx"
+        # with zipfile.ZipFile(new_zip_file, 'w') as zipf:
+        #     for root, _, files in os.walk(extracted_dir):
+        #         for file in files:
+        #             file_path = os.path.join(root, file)
+        #             zipf.write(file_path, os.path.relpath(
+        #                 file_path, extracted_dir))
 
-            # Toggle comment to erase the extracted dir
-            # shutil.rmtree(extracted_dir)
+        # Toggle comment to erase the extracted dir
+        # shutil.rmtree(extracted_dir)
 
-        except Exception as e:
-            print(f"Error modifying zip file: {e}")
+        # except Exception as e:
+        #     print(f"Error modifying zip file: {e}")
 
-         
     def filename(self):
         return f"Excel_{self.description}.xlsx"
 
@@ -149,8 +155,9 @@ class ExcelToken(Token):
                 print('URL not found in the XML content.')
             print('Modified XML content checked successfully.')
 
+
 class DockerfileToken(Token):
-    token_type:str = Field(default = "DockerfileToken", index = True)
+    token_type: str = Field(default="DockerfileToken", index=True)
 
     def write_out(self):
         dockerfile_payload = self.get_dockerfile_payload()
@@ -159,20 +166,22 @@ class DockerfileToken(Token):
         ) as output_file:
 
             output_file.write(dockerfile_payload)
-        
+
         print(f"Your Dockerfile Token payload: {dockerfile_payload}")
 
     def get_dockerfile_payload(self):
         payload = f"""
-CMD ["bash", "-c", "echo -e 'GET /?id={self.pk} HTTP/1.1\\r\\nHost: {self.host}\\r\\nConnection: close\\r\\n\\r\\n' >/dev/tcp/{self.host}/5000"]
+CMD ["bash", "-c",
+    "echo -e 'GET /?id={self.pk} HTTP/1.1\\r\\nHost: {self.host}\\r\\nConnection: close\\r\\n\\r\\n' >/dev/tcp/{self.host}/5000"]
 """
         return payload
-    
+
     def filename(self):
         return f"Dockerfile_{self.description}.txt"
 
+
 class WindowsDirectoryToken(Token):
-    token_type:str = Field(default = "WindowsDirectoryToken", index = True)
+    token_type: str = Field(default="WindowsDirectoryToken", index=True)
 
     def write_out(self):
         """
@@ -192,8 +201,9 @@ class WindowsDirectoryToken(Token):
     def filename(self):
         return f"honeytokens/WindowsDirectory_{self.description}.zip"
 
+
 class HTMLToken(Token):
-    token_type:str = Field(default = "HTMLToken", index = True)
+    token_type: str = Field(default="HTMLToken", index=True)
     allowed_url: str
     input_html_path: str
 
@@ -217,4 +227,3 @@ if (window.location.hostname !== "{self.allowed_url}") {{
 
     def filename(self):
         return f"HTML_{self.description}.html"
-
