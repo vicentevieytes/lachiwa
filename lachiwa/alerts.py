d4f0f40a6265cb327c73b3b8f22156ca9544fe37
vicente <vicentevieytes@hotmail.c>
2024-07-14 19:08:31 -0300

    restructure repo

---
+++ lachiwa/alerts.py
@@ -0,0 +1,24 @@
+from honeytokens import Token
+from datetime import datetime
+from redis_om import HashModel, Field
+from typing import Optional
+
+class HoneytokenAlert(HashModel):
+    token_id: str = Field(index=True)
+    timestamp: Optional[datetime] = Field(index=True)
+    remote_ip: str
+
+    def __init__(self, **data):
+        if 'timestamp' not in data:
+            data['timestamp'] = datetime.now()
+        super().__init__(**data)
+
+
+def log_alert(token: Token, alert: HoneytokenAlert):
+    with open(f"alerts/alert_log", "a") as alert_log:
+        alert_log.write(f"Alert_timestamp: {alert.timestamp.isoformat()} Token_type: {token.token_type} Description: {token.description} Creation_timestamp: {token.timestamp.isoformat()} Token_id: {token.pk} Remote_ip: {alert.remote_ip}\n")
+
+
+#TODO: def send_alert_email(self):
+
+
---
+++ lachiwa/lachiwa_cli.py
@@ -0,0 +1,124 @@
+import click
+from honeytokens import DockerfileToken, Token, URLToken, QRToken, ExcelToken, HTMLToken, WindowsDirectoryToken
+from redismanager import store_token
+
+
+def common_options(f):
+    """Common options for all tokens."""
+    f = click.option('--host', required=True,
+                     help='The host for the token.')(f)
+    f = click.option('--description', required=True,
+                     help='The description for the token.')(f)
+    f = click.option('--email', required=True,
+                     help='The email associated with the token.')(f)
+    return f
+
+
+@click.group()
+def create():
+    """Create a token."""
+    pass
+
+
+@create.command()
+@common_options
+def urltoken(host, description, email):
+    token = URLToken(
+        host = host,
+        description = description,
+        email = email
+    )
+    token.write_out()
+    store_token(token)
+    click.echo(f"Your URLToken: {token.url()}")
+
+
+@create.command()
+@common_options
+def qrtoken(host, description, email):
+    token = QRToken(
+        host = host,
+        description = description,
+        email = email
+    )
+    token.write_out()
+    store_token(token)
+    click.echo(f"Your QRToken file: {token.filename()}")
+
+@create.command()
+@common_options
+def exceltoken(host, description, email):
+
+    token = ExcelToken(
+        host = host,
+        description = description,
+        email = email
+    )
+    token.write_out()
+    store_token(token)
+    click.echo(f"Your ExcelToken file: {token.filename()}")
+
+
+@create.command()
+@common_options
+def dockertoken(host, description, email):
+    token = DockerfileToken(
+        host = host,
+        description = description,
+        email = email
+    )
+    token.write_out()
+    store_token(token)
+    click.echo(f"Your DockerfileToken file: {token.filename()}")
+
+
+@create.command()
+@common_options
+@click.option("--file", required=True, help="file path for input html file")
+@click.option("--allowed", required=True, help="The domain where your website should be hosted at")
+def htmltoken(host, description, email, file, allowed):
+    token = HTMLToken(
+        host = host,
+        description = description,
+        email = email,
+        allowed_url = allowed,
+        input_html_path = file)
+    token.write_out()
+    store_token(token)
+    click.echo(f"Your HTMLToken file: {token.filename()}")
+
+
+@create.command()
+@common_options
+def windowstoken(host, description, email):
+    token = WindowsDirectoryToken(
+        host = host,
+        description = description,
+        email = email
+    ) 
+    token.write_out()
+    store_token(token)
+    click.echo(f"Your WindowsToken file: {token.filename()}")
+
+
+@click.group()
+def get():
+    """Create a token."""
+    pass
+
+
+@get.command()
+def alerts():
+    print("traeme las alertas wachin")
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(create)
+cli.add_command(get)
+
+if __name__ == '__main__':
+    cli()
